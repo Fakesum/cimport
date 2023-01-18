a8e7e0a4a0dbcf22f56698691f41384c6decaed7
Fakesum <worldanvilbild@gmail.com>
2023-01-18 22:21:47 +0530

    Restructuring

--- cimport/c/compile.py
+++ cimport/c/compile.py
@@ -9,10 +9,6 @@ FunctionRegex = re.compile(r'.type([^,]+)')
 def find_functions(filename):
     return [func.replace("\t", "") for func in FunctionRegex.findall(open(f"__pycache__/cimport/{filename.split('.')[0]}.s").read())]
 
-def require_file(filename):
-    if not os.path.exists(filename):
-        raise RuntimeError("Failed to compile, try giving flags for gcc/msvc")
-
 def compile_c(filename, cpp, flags):
     compiler = ("g++" if cpp else "gcc")
     if os.name == "posix":
@@ -27,11 +23,13 @@ def compile_c(filename, cpp, flags):
         
 
         console.run_cmd([compiler, "-c", "-fPIC", "-save-temps=obj", filename, *flags[1], "-o", f"__pycache__/cimport/{filename.split('.')[0]}.o"])
-        require_file(f'__pycache__/cimport/{filename.split(".")[0]}.s')
-        require_file(f'__pycache__/cimport/{filename.split(".")[0]}.o')
+
+        console.require_file(f'__pycache__/cimport/{filename.split(".")[0]}.s')
+        console.require_file(f'__pycache__/cimport/{filename.split(".")[0]}.o')
 
         console.run_cmd([compiler, f"__pycache__/cimport/{filename.split('.')[0]}.o", "-shared", "-o",f"__pycache__/cimport/{filename.split('.')[0]}.compiled"])
-        require_file(f'__pycache__/cimport/{filename.split(".")[0]}.compiled')
+
+        console.require_file(f'__pycache__/cimport/{filename.split(".")[0]}.compiled')
 
         console.run_cmd(["rm", "-rf", f"__pycache__/cimport/{filename.split('.')[0]}.o"])
         console.run_cmd(["rm", "-rf", f"__pycache__/cimport/{filename.split('.')[0]}.ii"])
