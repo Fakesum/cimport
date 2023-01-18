07f356ec2bd31a2c93981a459b5d6a3aa9e8e041
Fakesum <worldanvilbild@gmail.com>
2023-01-18 16:42:22 +0530

    Changed api, from cimport import cimport

--- cimport/__init__.py
+++ cimport/__init__.py
@@ -1,13 +1,12 @@
 __version__ = "0.0.1b"
 __name__ = "cimport"
 
-
 import os
 import pathlib
 from .compile import compile_c, find_functions
 from .program import CppProgram
 
-def import_program(filename: pathlib.Path | str, cpp=True, flags=[[], []]):
+def cimport(filename: pathlib.Path | str, cpp=True, flags=[[], []]):
     if (not os.path.exists(os.path.join("__pycache__/cimport", f"""{filename.split(".")[0]}.s"""))) or (open(f"__pycache__/cimport/{filename.split('.')[0]}.ver").read() != open(filename).read()):
         return compile_c(filename, cpp, flags)
     
