f093bf58e57db828d226d6b1c6159cabe7cc7edf
Fakesum <worldanvilbild@gmail.com>
2023-01-19 22:49:50 +0530

    Tests

--- tests/speed_test/speed_test.rs
+++ tests/speed_test/speed_test.rs
@@ -1,5 +1,8 @@
+#[no_mangle]
 fn return_int()->i32{ return 1 }
 
+#[no_mangle]
 fn return_float() -> f32{ return 1.0; }
 
-fn return_string()->str{ return "This works!"; }
+#[no_mangle]
+fn return_string()->&'static str{ return "This works!"; }
