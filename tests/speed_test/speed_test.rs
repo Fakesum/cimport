0026f29633e2cd5df1731cccf37065cd1f526e95
Fakesum <worldanvilbild@gmail.com>
2023-01-19 22:35:44 +0530

    Fixed Test Script

--- tests/speed_test/speed_test.rs
+++ tests/speed_test/speed_test.rs
@@ -1,5 +1,5 @@
-fn return_int()->i32{ 1 }
+fn return_int()->i32{ return 1 }
 
-fn return_float() -> f32{ 1.0; }
+fn return_float() -> f32{ return 1.0; }
 
-fn return_string()->str{ "This works!"; }
+fn return_string()->str{ return "This works!"; }
