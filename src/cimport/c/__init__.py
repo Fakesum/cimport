97d64b7a8cd53e357146bd7af7511e808c8c841d
Fakesum <worldanvilbild@gmail.com>
2023-01-19 17:49:00 +0530

    Removed Debug, Print statements

--- src/cimport/c/__init__.py
+++ src/cimport/c/__init__.py
@@ -5,13 +5,9 @@ from .program import CppProgram, CProgram
 
 def c_import(filename: pathlib.Path | str, flags=[[], []]):
     cpp = filename.split(".")[1] in ['C','cpp', 'c++']
-    print("[DEBUG] Importing")
     if (not os.path.exists(f"""__pycache__/cimport/{filename}.s""")) or (open(f"__pycache__/cimport/{filename}.ver").read() != open(filename).read()):
-        print("[DEBUG] Compiling")
         return compile_c(filename, cpp, flags)
     
-    print("[DEBUG] geting functions.")
-
     if cpp: 
         return CppProgram(f"__pycache__/cimport/{filename}.compiled", find_functions(filename))
     elif filename.split(".")[1] == 'c':
