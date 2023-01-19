59b12078e23dc20b3f33786a7e3c692cfe730f73
Fakesum <worldanvilbild@gmail.com>
2023-01-19 21:23:48 +0530

    Better Build Command

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-python -m build && pip uninstall cimport && pip install dist/cimport-0.0.2a0-py3-none-any.whl
+python -m build
+yes | pip uninstall cimport
+pip install dist/cimport-0.0.2a0-py3-none-any.whl
