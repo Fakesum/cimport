6c1a3db8370914006d8de28ab90335f582a86a34
Fakesum <worldanvilbild@gmail.com>
2023-01-28 11:20:48 +0530

    Version File

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+if [ "x$1" == "x" ]; then
+    set $(cat version.txt)
+fi
+sed -i.bak "1 s/.*/__version__ = \"$1\"/" src/cimport/__init__.py
+sed -i.bak "7 s/.*/version = \"$1\"/" pyproject.toml
 rm -rf dist
 python -m build
 yes | pip uninstall cimport
