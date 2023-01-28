079e31d95f6649aa26cd6bde17ac0790710f0a48
Fakesum <worldanvilbild@gmail.com>
2023-01-28 22:56:07 +0530

    Javascript

--- src/cimport/javascript/__init__.py
+++ src/cimport/javascript/__init__.py
@@ -1,14 +1,21 @@
+import js2py
+from importlib.util import spec_from_file_location, module_from_spec
+import os
 
 class JsProgram:
-    def __init__(self, program) -> None:
-        self._program = program
+    def __init__(self, filename, name) -> None:
+        spec = spec_from_file_location(filename.split(".")[0], filename + ".py")
+        self._program = module_from_spec(spec)
+        spec.loader.exec_module(self._program)
     
-    def get(name, *args):
-        return eval(f"self._program.{name}")
+    def get(self, name, *args):
+        return eval(f"self._program.PyJsHoisted_{name}_")
+    
+    def __getattr__(self, __name):
+        return self.get(__name)
 
 def js_import(filename):
-    import js2py
-    import importlib
-    js2py.translate_file(filename, filename + ".js")
+    if (not (os.path.exists(f"""__pycache__/cimport/{filename + ".py"}"""))) or (open("""""")):
+        js2py.translate_file(filename, filename + ".py")
 
-    return JsProgram(importlib.import_module(filename + ".js"))
+    return JsProgram(filename, filename.split(".")[0])
