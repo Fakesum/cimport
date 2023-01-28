6dc9cc4b4bc4715a057ff306c93c71a4f392f00f
Fakesum <worldanvilbild@gmail.com>
2023-01-28 11:57:25 +0530

    Fix

--- src/cimport/rust/__init__.py
+++ src/cimport/rust/__init__.py
@@ -9,7 +9,8 @@ def pre_processor(filename):
     for line in lines:
         if line.startswith("fn") and ((lines.index(line) == 0) or (lines[lines.index(line)-1] != "#[no_mangle]")):
             n_file += "#[no_mangle]\n"
-        n_file += line 
+        n_file += line + ("\n" if line != "\n" else "")
+    n_file = n_file[:-1]
     
     open(filename, "w+").write(n_file)
 
--- tests/speed_test/speed_test.rs
+++ tests/speed_test/speed_test.rs
@@ -6,13 +6,3 @@ fn return_float() -> f32{ return 1.0; }
 
 #[no_mangle]
 fn return_string()->&'static str{ return "This works!"; }
-
-
-
-
-
-
-
-
-
-
