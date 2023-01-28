7fd2c52db28158fdb2a18e9a403cf1fd8db418a9
Fakesum <worldanvilbild@gmail.com>
2023-01-28 10:16:09 +0530

    Proper Speed test

--- tests/speed_test/speed_test.py
+++ tests/speed_test/speed_test.py
@@ -4,19 +4,44 @@ from cimport import c_import, rust_import
 
 def timeit(f):
     st = time.time()
-    return (f(), time.time() - st)
+    return (f(), time.time() - st) 
+
+def run_test(program, command: str):
+    res = timeit(lambda: program.get(command)())
+    print("took", res[1], "Seconds to run", command, "returned", res[0])
 
 def c_test():
     c_program = timeit(lambda: c_import("speed_test.c"))
     print("took", c_program[1], "seconds to load c program")
 
+    c_program = c_program[0]
+
+    run_test(c_program, "return_int")
+    run_test(c_program, "return_float")
+    run_test(c_program, "return_double")
+    run_test(c_program, "return_string")
+
 def cpp_test():
     cpp_program = timeit(lambda: c_import("speed_test.cpp"))
     print("took", cpp_program[1], "seconds to load c++ program")
 
+    cpp_program = cpp_program[0]
+
+    run_test(cpp_program, "return_int")
+    run_test(cpp_program, "return_float")
+    run_test(cpp_program, "return_double")
+    run_test(cpp_program, "return_string")
+
 def rust_test():
     rust_program = timeit(lambda: rust_import("speed_test.rs"))
     print("took", rust_program[1], "seconds to load rust program")
+
+    rust_program = rust_program[0]
+
+    run_test(rust_program, "return_int")
+    run_test(rust_program, "return_float")
+    run_test(rust_program, "return_string")
+
 
 TESTS = [
     c_test,
--- tests/speed_test/speed_test.rs
+++ tests/speed_test/speed_test.rs
@@ -1,5 +1,18 @@
+#[no_mangle]
 fn return_int()->i32{ return 1 }
 
+#[no_mangle]
 fn return_float() -> f32{ return 1.0; }
 
+#[no_mangle]
 fn return_string()->&'static str{ return "This works!"; }
+
+
+
+
+
+
+
+
+
+
