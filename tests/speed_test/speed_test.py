2754e3b1a9a13006955130ea04a8dc06d24f4fd0
Fakesum <worldanvilbild@gmail.com>
2023-02-03 22:53:22 +0530

    Restructring Tests

--- tests/speed_test/speed_test.py
+++ tests/speed_test/speed_test.py
@@ -12,7 +12,7 @@ def run_test(program, command: str, _type = ctypes.c_int, arg_type = []):
     print("took", res[1], "Seconds to run", command, "returned", res[0])
 
 def c_test():
-    c_program = timeit(lambda: c_import("speed_test.c"))
+    c_program = timeit(lambda: c_import("tests/speed_test.c"))
     print("took", c_program[1], "seconds to load c program")
 
     c_program = c_program[0]
@@ -23,7 +23,7 @@ def c_test():
     run_test(c_program, "return_string", ctypes.c_char_p)
 
 def cpp_test():
-    cpp_program = timeit(lambda: c_import("speed_test.cpp"))
+    cpp_program = timeit(lambda: c_import("tests/speed_test.cpp"))
     print("took", cpp_program[1], "seconds to load c++ program")
 
     cpp_program = cpp_program[0]
@@ -34,7 +34,7 @@ def cpp_test():
     run_test(cpp_program, "return_string", ctypes.c_char_p)
 
 def rust_test():
-    rust_program = timeit(lambda: rust_import("speed_test.rs"))
+    rust_program = timeit(lambda: rust_import("tests/speed_test.rs"))
     print("took", rust_program[1], "seconds to load rust program")
 
     rust_program = rust_program[0]
@@ -44,7 +44,7 @@ def rust_test():
     run_test(rust_program, "return_string", ctypes.c_char_p)
 
 def js_test():
-    js_program = timeit(lambda: js_import("speed_test.js"))
+    js_program = timeit(lambda: js_import("tests/speed_test.js"))
     print("took", js_program[1], "seconds to load rust program")
 
     js_program = js_program[0]
